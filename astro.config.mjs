fba8e8269de6156f704a420d679debb34d2268c7
Andrew Walpole <walpolea@gmail.com>
2024-04-16 18:57:40 -0700

    add og image

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vue from "@astrojs/vue";
 const isProd = import.meta.env.PROD;
 const isDev = import.meta.env.DEV;
 
-const PROD_URL = 'https://layout-breakouts-builder.vercel.app/';
+const PROD_URL = 'https://layout-breakouts-builder.vercel.app';
 const DEFAULT_URL = isDev ? 'http://localhost:4321' : PROD_URL;
 
 // https://astro.build/config
